d1ecd696e3eb7d6f28c56727e534750d1a1bcd39
RHsanto <rakibulsanto789@gmail.com>
2022-06-24 08:02:34 +0600

    add google font

--- App.js
+++ App.js
@@ -20,18 +20,8 @@ export default function App() {
   // here load google font
 
   let [fontsLoaded] = useFonts({
-    SourceSansPro_200ExtraLight,
-    SourceSansPro_200ExtraLight_Italic,
-    SourceSansPro_300Light,
-    SourceSansPro_300Light_Italic,
     SourceSansPro_400Regular,
-    SourceSansPro_400Regular_Italic,
-    SourceSansPro_600SemiBold,
-    SourceSansPro_600SemiBold_Italic,
     SourceSansPro_700Bold,
-    SourceSansPro_700Bold_Italic,
-    SourceSansPro_900Black,
-    SourceSansPro_900Black_Italic,
   });
   if (!fontsLoaded) {
     return <Text>Font loading ....</Text>;
---
+++ src/theme/typography.js
@@ -0,0 +1,4 @@
+export const typography ={
+  primary:' SourceSansPro_400Regular',
+  primaryBold:'SourceSansPro_700Bold'
+}
