25b30a320204e50a8145d046318c692755baccd1
RHsanto <rakibulsanto789@gmail.com>
2022-06-24 08:29:33 +0600

    add text components and text-presets

---
+++ src/components/text/text.preset.js
@@ -0,0 +1,39 @@
+import { colors } from "../../theme/color";
+import { typography } from "../../theme/typography";
+
+
+const BASE ={
+  fontFamily:typography.primary,
+  fontSize:16,
+  color:colors.violet,
+}
+
+const BOLD ={
+  fontFamily:typography.primaryBold,
+  color:colors.violet,
+}
+
+export const presets ={
+  default : BASE,
+  bold:BOLD,
+  h1:{
+    ...BOLD,
+    fontSize:34,
+  },
+  h2:{
+    ...BOLD,
+    fontSize:30,
+  },
+  h3:{
+    ...BOLD,
+    fontSize:24,
+  },
+  h4:{
+    ...BASE,
+    fontSize:18,
+  },
+  small:{
+    ...BASE,
+    fontSize:14
+  }
+}
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,12 +1,14 @@
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import Text from "../components/text/text";
+
 import { typography } from "../theme/typography";
 
 export default function Home(){
   return (
     <SafeAreaView>
       <ScrollView>
-      <View>
-      <Text >
+      <View >
+      <Text preset="h3">
         Hello World
       </Text>
     </View>
